a52d8c83374ffd6c4653f8417cf8c0c9e4252de4
HanGyeolN <ghnruf@naver.com>
2020-02-16 17:16:58 +0900

    C Piscine Libft | My C Library

---
+++ libs/ft_string/ft_strncat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hna <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/27 14:52:24 by hna               #+#    #+#             */
+/*   Updated: 2020/01/27 22:31:44 by hna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *src)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
+
+char			*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int i;
+	unsigned int dest_len;
+
+	i = 0;
+	dest_len = ft_strlen(dest);
+	while (src[i] != '\0' && i < nb)
+	{
+		dest[i + dest_len] = src[i];
+		i++;
+	}
+	dest[i + dest_len] = '\0';
+	return (dest);
+}
